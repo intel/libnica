af7f2e5de95afae4da27da349c9fe4b4fa9d024a
Ikey Doherty <michael.i.doherty@intel.com>
2016-03-31 15:05:55 +0100

    Add a simple ABI check script
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

---
+++ abi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This file is part of libnica.
+#
+# Copyright (C) 2016 Intel Corporation
+#
+# libnica is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+# 
+
+function check_abi()
+{
+    whence="$1"
+    nm -g $1 |grep " T "|awk '{print $3}'|grep -v "^_init" |grep -v "^_fini" |sort
+}
+
+check_abi .libs/*.so.*.* > newsyms
+diff -ruNb current_symbols newsyms
+if [[ $? -eq 0 ]]; then
+        rm newsyms
+        exit 0
+fi
+
+echo "ABI change detected"
+exit 1
