f30fb82237e659349f7036dd6fd246a5cfdb82db
Ikey Doherty <michael.i.doherty@intel.com>
2016-11-03 13:23:13 +0000

    hashmap: Fix signature issue & item dereference
    The signature was incorrect for inserting buckets, as we used an int, not
    a boolean. Another issue resolved with this change is the potential
    dereferencing of a null pointer by not having checked first if item was
    NULL when setting the next pointer.
    
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- src/hashmap.c
+++ src/hashmap.c
@@ -119,8 +119,8 @@ static inline unsigned nc_hashmap_get_hash(NcHashmap *self, const void *key)
         return hash;
 }
 
-static bool nc_hashmap_insert_bucket(NcHashmap *self, NcHashmapEntry *buckets, int n_buckets,
-                                     unsigned hash, const void *key, void *value)
+static int nc_hashmap_insert_bucket(NcHashmap *self, NcHashmapEntry *buckets, int n_buckets,
+                                    unsigned hash, const void *key, void *value)
 {
         NcHashmapEntry *row = &(buckets[hash % (unsigned)n_buckets]);
         NcHashmapEntry *head = NULL;
@@ -434,10 +434,12 @@ bool nc_hashmap_iter_next(NcHashmapIter *citer, void **key, void **value)
                         }
                         item = &(map->buckets[iter->bucket]);
                 }
-                if (item && item->occ) {
-                        goto success;
+                if (item) {
+                        if (item->occ) {
+                                goto success;
+                        }
+                        item = item->next;
                 }
-                item = item->next;
         }
         return false;
 
